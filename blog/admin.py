a0159b4aeea87abc6d8aaa5b2ad90e266a3e41d1
mahdi921 <mahdi.afshar921@gmail.com>
2024-12-24 00:24:41 +0330

    registered post table in admin.py to be able to view more details in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date', 'updated_date')
+    list_filter = ('status', 'created_date')
+    search_fields = ('title', 'content')
+    # ordering = ['-created_date']
+    actions = ['make_published']
+    empty_value_display = '-empty-'
+    date_hierarchy = 'created_date'
+
+# admin.site.register(Post, PostAdmin)
