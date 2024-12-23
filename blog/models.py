814154046108d45cfed78019ade6fe6eaa6d1031
mahdi921 <mahdi.afshar921@gmail.com>
2024-12-23 20:12:35 +0330

    created post table in database and then added some more fields to it

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
+    
