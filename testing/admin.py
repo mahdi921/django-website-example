a11f0c30aa1efead23494745d5496daccc3c5600
mahdi921 <mahdi.afshar921@gmail.com>
2024-12-24 05:29:31 +0330

    Registered contact model in admin.py to be able to view it on admin page

--- testing/admin.py
+++ testing/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from testing.models import Contact
 
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_date')
+    list_filter = ('created_date',)
+    search_fields = ('name', 'email', 'subject')
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
