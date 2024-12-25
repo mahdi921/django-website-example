25046ad2b1276ee9055c2777602e76012692ac82
mahdi921 <mahdi.afshar921@gmail.com>
2024-12-26 01:50:12 +0330

    added the ability to show only the posts that have their published datetime past datetime now now the posts have view counts per request

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
+from datetime import datetime
 
 # Create your views here.
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(published_date__lte=datetime.now(), status=True)
+    # posts.counted_views += 1
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog-home.html', context)
 
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request,pid):
+    post = get_object_or_404(Post, id=pid)
+    post.counted_views += 1
+    post.save()
+    context={
+        'post': post
+    }
+    return render(request, 'blog/blog-single.html', context)
