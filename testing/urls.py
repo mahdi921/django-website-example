9ca12ae94957e6289373ac64434f37d3de699a09
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-03 11:11:59 +0330

    added form test files and forms.py and url and form test view to work with forms

--- testing/urls.py
+++ testing/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index_view, about_view, contact_view
+from .views import index_view, about_view, contact_view, form_test_view
 
 app_name = 'testing'
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('about', about_view, name='about'),
-    path('contact', contact_view, name='contact')
+    path('contact', contact_view, name='contact'),
+    path('form-test', form_test_view, name='form-test'),
 ]
--- testing/views.py
+++ testing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+from testing.models import Contact
+from testing.forms import ContactForm
 
 def index_view(request):
     return render(request, 'testing/index.html')
@@ -12,3 +13,21 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, 'testing/contact.html')
+
+
+def form_test_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            print(name, email, subject, message)
+            #form.save()
+            return HttpResponse('Form submitted successfully')
+        else:
+            return HttpResponse('Form is not valid')
+    else:
+        form = ContactForm()
+    return render(request, 'testing/form-test.html', {'form':form})
