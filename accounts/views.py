06db56eea8eed292276bd54457272939e91a37e0
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-09 20:34:38 +0330

    added logout and register url and logout_view and register_view to accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'accounts/login.html')
+
+def logout_view(request):
+    return render(request, 'accounts/logout.html')
+
+def register_view(request):
+    return render(request, 'accounts/register.html')
