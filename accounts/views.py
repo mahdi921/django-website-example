bd114ae374ca6c5f54a28ac31204e21ea7fb7f55
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-11 12:13:37 +0330

    completed basic login with django authentication form

--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
 def login_view(request):
-    msg = ''
-    if request.method == 'POST':
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.add_message(request, messages.SUCCESS,
-                                 'Login successful, redirecting to home page...')
-            return redirect('/')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.add_message(request, messages.SUCCESS,
+                                         'Login successful, redirecting to home page...')
+                    return redirect('/')
+
         else:
-            messages.add_message(request, messages.ERROR,
-                                 'Login failed, check your username and password')
-    return render(request, 'accounts/login.html')
+            form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form': form})
+    else:
+        messages.add_message(request, messages.ERROR,
+                             "You are already logged in, redirecting to home page...")
+        return redirect('/')
+
 
 def logout_view(request):
     return render(request, 'accounts/logout.html')
